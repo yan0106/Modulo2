7788115447f2e5136e8ffd9183ce0a40c6841f0a
yan0106 <yandiradiaz@htomail.com>
2022-03-31 20:30:00 -0300

    se agrega archivo js

--- JavaScrip/busqueda_en_arrays.js
+++ JavaScrip/busqueda_en_arrays.js
@@ -5,7 +5,7 @@ let arreglo = [10, 11, 3, 20, 5];
 let mayorQueDiez = arreglo.filter(element => element > 10);
 
 console.log(mayorQueDiez) // resultado esperado: [11, 20]
-
+/* 
 // Array.find() : para encontrar el primer elemento que cumple cierta condición. 
 
 let arreglo = [10, 11, 3, 20, 5];
@@ -16,7 +16,7 @@ console.log(existeElementoMayorQueDiez) // resultado esperado: 11
 
 // Array.includes() : determina si un arreglo incluye un valor específico y devuelve verdadero o falso según corresponda.
 
-
+ */
 
 
 

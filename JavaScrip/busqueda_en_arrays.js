c7e2fe0f9968c0c6db699649b9b38156fa4dfe84
yan0106 <yandiradiaz@htomail.com>
2022-03-31 21:00:30 -0300

    modificacion en archivo busqueda_en_arrays.js

--- JavaScrip/busqueda_en_arrays.js
+++ JavaScrip/busqueda_en_arrays.js
@@ -5,7 +5,9 @@ let arreglo = [10, 11, 3, 20, 5];
 let mayorQueDiez = arreglo.filter(element => element > 10);
 
 console.log(mayorQueDiez) // resultado esperado: [11, 20]
-/* 
+
+
+
 // Array.find() : para encontrar el primer elemento que cumple cierta condición. 
 
 let arreglo = [10, 11, 3, 20, 5];
@@ -14,9 +16,29 @@ let existeElementoMayorQueDiez = arreglo.find(element => element > 10);
 
 console.log(existeElementoMayorQueDiez) // resultado esperado: 11
 
+
+
 // Array.includes() : determina si un arreglo incluye un valor específico y devuelve verdadero o falso según corresponda.
 
- */
+let arreglo = [10, 11, 3, 20, 5];
+
+let incluyeVeinte = arreglo.includes(20);
+
+console.log(incluyeVeinte) // resultado esperado: true
+
+
+
+// Array.indexOf() : El método indexOf() devuelve el primer índice encontrado de un elemento específico. 
+// Devuelve -1 si el elemento no existe en el arreglo.
+
+let arreglo = [10, 11, 3, 20, 5];
+
+let indiceDeTres = arreglo.indexOf(3);
+
+console.log(indiceDeTres) // resultado esperado: 2
+
+
+
 
 
 

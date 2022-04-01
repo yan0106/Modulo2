a27f9bd2aee14db78edc1f6e69e90cd0d32c7591
yan0106 <yandiradiaz@htomail.com>
2022-03-31 21:07:08 -0300

    modificacion en archivos js

--- JavaScrip/ciclos_for.js
+++ JavaScrip/ciclos_for.js
@@ -1,48 +1,53 @@
-/* Ejemplo de ciclo for */
+// Ejemplo de ciclo for:
 
-/* var miArreglo = [];
+var miArreglo = [];
 
 for (var i = 0; i <= 10; i = i++) {
     miArreglo.push(i);
 }
-console.log(miArreglo); */
+console.log(miArreglo);
 
-/* Otro ejemplo de ciclo for */
 
-/* for (var i = 5; i < 10; i++) {
+// Otro ejemplo de ciclo for:
+
+for (var i = 5; i < 10; i++) {
     console.log("Número:" + i);
-} */
+}
 
-/* Ciclo for: números impares */
 
-/* var miArreglo = [];
+// Ciclo for: números impares:
+
+var miArreglo = [];
 
 for (var i = 0; i <= 10; i += 2) {
     miArreglo.push(i);
 }
-console.log(miArreglo); */
+console.log(miArreglo);
 
-/*Ciclo for: contar hacia atrás */
 
-/* for (var i = 15; i >= 10; i--) {
+// Ciclo for: contar hacia atrás:
+
+for (var i = 15; i >= 10; i--) {
     console.log(i);
 }
- */
-/*Iterar sobre un arreglo con un ciclo "for":*/
 
-/* var miArreglo = [4, 6, 8, 2];
+
+// Iterar sobre un arreglo con un ciclo "for":
+
+var miArreglo = [4, 6, 8, 2];
 var total = 0;
 
 for (var i = 0; i < miArreglo.length; i++) {
-total += miArreglo[i];
+    total += miArreglo[i];
 }
 
-console.log(total); */
+console.log(total);
 
-/* Otro ejemplo de iteración con un ciclo for sobre un arreglo */
 
-var lenguajes = [ "JavaScritp", "Paython", "Java", "C++"];
+// Otro ejemplo de iteración con un ciclo for sobre un arreglo:
 
-for (var i = 0; i < lenguajes.length; i++ ) {
-console.log(lenguajes[i].toUpperCase()); /* toUpperCase --> para convertir en mayúscula */
+var lenguajes = ["JavaScritp", "Paython", "Java", "C++"];
+
+for (var i = 0; i < lenguajes.length; i++) {
+    console.log(lenguajes[i].toUpperCase()); /* toUpperCase --> para convertir en mayúscula */
 }
